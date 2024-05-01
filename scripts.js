9631c1bce9b0e7ef6ebe9aaac0a478a0caab3df8
victorpedrosa10 <victorpedrosa@hotmail.com.br>
2024-05-01 12:04:44 -0300

    conversão uds finalizada

--- scripts.js
+++ scripts.js
@@ -3,8 +3,23 @@ const convertButton = document.querySelector(".convert-button")
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
+
     const dolarToday = 5.00
+
     const convertedValue = inputCurrencyValue/dolarToday
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR",{
+        style:"currency", 
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US",{
+        style:"currency", 
+        currency: "USD"
+    }).format(convertedValue)
+
     console.log(convertedValue)
 }
 
