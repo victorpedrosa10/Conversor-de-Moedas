d6538ffb95e303559e66b6b4722b83919041d8cc
victorpedrosa10 <victorpedrosa@hotmail.com.br>
2024-05-01 11:10:49 -0300

    js adicionado

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const convertButton = document.querySelector(".convert-button")
+
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const dolarToday = 5.00
+    const convertedValue = inputCurrencyValue/dolarToday
+    console.log(convertedValue)
+}
+
+convertButton.addEventListener("click", convertValues)
