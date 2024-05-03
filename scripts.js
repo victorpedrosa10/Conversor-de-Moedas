700b7fc03ea14c225927a7bbcc24a4f4e9dad65a
victorpedrosa10 <victorpedrosa@hotmail.com.br>
2024-05-03 08:26:58 -0300

    trocar a moeda

--- scripts.js
+++ scripts.js
@@ -11,26 +11,36 @@ function convertValues() {
     const dolarToday = 5.00
     const euroToday = 6.00
 
-        if(currencySelect.value == "dolar"){
-            currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US",{
-                style:"currency", 
-                currency: "USD"
-            }).format(inputCurrencyValue/dolarToday)
-        }
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
 
-        if(currencySelect.value == "euro"){
-            currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE",{
-                style:"currency", 
-                currency: "EUR"
-            }).format(inputCurrencyValue/euroToday)
-        }
+    if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
 
-    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR",{
-        style:"currency", 
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)
 
     console.log(convertedValue)
+
+    
 }
 
+// function changeCurrency() {
+//     console.log('trocou a moeda')
+// }
+// currencySelect.addEventListener("change", changeCurrency())
+
 convertButton.addEventListener("click", convertValues)
+
+
+
